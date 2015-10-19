108ff29f49d2c470320dfa076baedce8fdcba276
James Rooke <thepoltergeist23@gmail.com>
2015-10-19 22:01:13 +0800

    add redirect to root

--- app/controllers/contact_me_controller.rb
+++ app/controllers/contact_me_controller.rb
@@ -1,5 +1,6 @@
 class ContactMeController < ApplicationController
   def send_message
     UserMailer.contact_me(params[:email].downcase, params[:subject], params[:message])
+    redirect_to :back
   end
 end
