6956c2520bd6d11b3042597642ea8d610c54be62
James Rooke <thepoltergeist23@gmail.com>
2015-10-20 00:05:09 +0800

    fix gmail smtp ports and add domain

--- config/initializers/mailer_setup.rb
+++ config/initializers/mailer_setup.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :port           => 465,
-  :address        => 'smtp.gmail.com',
-  :user_name      => ENV['gmail_username'],
-  :password       => ENV['gmail_password'],
-  :authentication => :plain,
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => ENV['gmail_username'],
+  :password             => ENV['gmail_password'],
+  :authentication       => :plain,
   :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
