ac5e9a9af5568aab5312de5c9cb2ce2030fbe56b
james robert rooke <james.rooke@paramount.com.ph>
2015-10-23 15:53:57 +0800

    fix username and password environment variables

--- config/initializers/mailer_setup.rb
+++ config/initializers/mailer_setup.rb
@@ -1,10 +1,7 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => :login,
-  :enable_starttls_auto => true
-}
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.mandrillapp.com",
+  :port => 587,
+  :user_name => ENV["user_name"],
+  :password => ENV["password"]
+}
